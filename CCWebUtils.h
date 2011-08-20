9a0da35d7aeba8651852a417b42c1622e101388b
James Womack <james@cirrostratusco.com>
2011-08-20 14:01:27 -0700

    Added forced error if ARC is activated
    Signed-off-by: James Womack <james@cirrostratusco.com>

--- CCWebUtils.h
+++ CCWebUtils.h
@@ -9,14 +9,13 @@
 #import <Foundation/Foundation.h>
 
 
-@interface CCWebUtils : NSObject {
-
-}
+@interface CCWebUtils : NSObject
 
 + (NSString*)urlEscape:(NSString *)unencodedString;
 + (NSString*)addQueryStringToUrl:(NSString *)url params:(NSDictionary *)params;
 + (NSString*)postToUrl:(NSString *)urlString params:(NSDictionary *)params;
 + (NSData *)ut8postToUrl:(NSString *)urlString params:(NSDictionary *)params;
++ (BOOL)validateEmail:(NSString *)candidate;
 
 
 @end
--- CCWebUtils.m
+++ CCWebUtils.m
@@ -8,6 +8,17 @@
 
 #import "CCWebUtils.h"
 
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+
+#ifdef __OBJC_GC__
+#error CCWebUtils does not support Objective-C Garbage Collection
+#endif
+
+#if __has_feature(objc_arc)
+#error CCWebUtils does not support Objective-C Automatic Reference Counting (ARC)
+#endif
 
 @implementation CCWebUtils
 
@@ -82,8 +93,23 @@
 	NSString *s = [[NSString alloc] initWithBytes:[returnData2 bytes] length:[returnData2 length] encoding:NSUTF8StringEncoding];	
 	
 	ILogPlus(@"%@, %@",s,returnData2);
-	
+
 	return [s autorelease];
+}
+
++ (BOOL)validateEmail:(NSString *)candidate; {
+    NSString *emailRegEx =
+	@"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+	@"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+	@"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+	@"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+	@"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+	@"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+	@"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+	
+	
+	NSPredicate *regExPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
+	return [regExPredicate evaluateWithObject:candidate];
 }
 
 @end
